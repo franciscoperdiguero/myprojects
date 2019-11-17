b5075dd6da76eccdf18206a550f25da0c53c9db0
Francisco Perdiguero <fran_n_45@hotmail.com>
2019-11-17 14:50:14 +0100

    Finished the static part

---
+++ Web Development/MiniProjects/To-Do WebSite with Persistence Data/script.js
@@ -0,0 +1,53 @@
+
+const inputText = $(".inputText input");
+const button = $(".inputText #btn1")
+
+const notCompleted = $(".remainingTask");
+const completed = $(".completedTask");
+
+
+inputText.on("keyup", (e) => {
+    addATask(e);
+});
+
+button.on("click", (e) => {
+    addATask(e);
+});
+
+$(document).on("click", ".task .fa-trash", (e) => {
+    removeTask(e);
+});
+
+$(document).on("click", ".task .fa-check", (e) => {
+    var p = $(e.currentTarget.parentNode.parentNode);
+    p.find(".fa-check").remove();
+
+    p.fadeOut(function () {
+        p.remove();
+        completed.append(p);
+        p.fadeIn();
+    });
+});
+
+
+function removeTask(e) {
+    var p = $(e.currentTarget.parentNode.parentNode);
+    p.fadeOut(function () {
+        p.remove();
+    });
+}
+
+function addATask(e) {
+    if ((e.keyCode == 13 || e.button == 0) && inputText.val() != "") {
+
+        let tick = $("<i class=\"fas fa-check\"></i>");
+        let trash = $("<i class=\"fas fa-trash\"></i>");
+        let icons = $("<div class='wrapIcons'></div>").append(tick, trash);
+        let name = $("<label></label>").append(inputText.val());
+
+        let task = $("<div class='task'></div>").append(name, icons);
+        notCompleted.append(task);
+
+        inputText.val("");
+    }
+};
